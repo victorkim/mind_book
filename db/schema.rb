d138f9b44a85d24432b338d21b41fc1fcbca96c0
Victor Kim <victor.eun@gmail.com>
2024-05-14 07:54:06 -0300

    Reverting to 4b3a32b commit to use Devise for user authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_25_160802) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_23_004007) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -52,13 +52,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_25_160802) do
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.text "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
