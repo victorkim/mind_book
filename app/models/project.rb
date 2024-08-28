424fa00a7d189bd60cddcacac352acd99340cd6e
Victor Kim <victor.eun@gmail.com>
2024-08-28 16:35:37 -0300

    Migrated db and added 2 columns to the Projects table (start_date and end_date). Added validations in the model, so that Projects cannot be created without Start and End Dates. I've also included both fields to the _form, _project_detail and _project_general partials and permitted them in the Controller's strong parameters

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ApplicationRecord
-    validates_presence_of :name
+    validates :name, presence: true
     has_rich_text :description
+    validates :start_date, presence: true
+    validates :end_date, presence: true    
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_21_100343) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_28_190734) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -54,6 +54,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_21_100343) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "start_date"
+    t.date "end_date"
   end
 
   create_table "users", force: :cascade do |t|
