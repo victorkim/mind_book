e5da1f8b77b0e12d11f8b832f51342234d95e789
Victor Kim <victor.eun@gmail.com>
2025-02-03 10:01:22 -0300

    creating, editing and deleting comments from both channel and project pages is workinggit add -A now, just add the _comments_new_form and edit partials for comments in the channel page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,61 +1,34 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :weekly] #checks if user is authenticated whenever they try to run any operation besides index
-  before_action :set_project, only: [:create] #This method is called before each action. It finds the project that the comment belongs to, using the project_id from the URL.
+  before_action :set_parent, only: [:create, :edit, :update, :destroy, :index, :weekly] #This method is called before each action. It finds the parent that the comment belongs to, using the project_id/channel_id from the URL.
   before_action :set_comment, only: [:edit, :update, :destroy] #This method is called before the edit, update, and destroy actions. It finds the specific comment within the project by its id.
   
   def index
-      @comments = @project.comments #GET request that displays all comments for a specific project (which is specified using set_project)
+      @comments = @parents.comments #GET request that displays all comments for a specific project (which is specified using set_project)
   end
       
   def create
-    # 1) Check which nested route is being used
-    if params[:project_id]
-      @project = Project.find(params[:project_id])
-      @comment = @project.comments.build(comment_params)
-    elsif params[:channel_id]
-      @channel = Channel.find(params[:channel_id])
-      @comment = @channel.comments.build(comment_params)
-    else
-      # If there's no nested route, you could handle an error or just build a comment from comment_params
-      @comment = Comment.new(comment_params)
-    end
-  
-    # 2) Fill in additional fields (e.g. user, date).
+    # Build the comment off the parent (either Project or Channel)
+    @comment = @parent.comments.build(comment_params)
     @comment.user = current_user
     @comment.date ||= Date.today
-  
-    # If user is creating from a Channel page, we also need a Project to be selected from the form:
-    if @channel && comment_params[:project_id].present?
+    # If creating from a Channel, make sure the comment gets associated with a Project as well
+    if @parent.is_a?(Channel) && comment_params[:project_id].present?
       @comment.project = Project.find(comment_params[:project_id])
     end
-  
-    # If user is creating from a Project page, channel_id might also be assigned in the form
-    if @project && comment_params[:channel_id].present?
+
+    # If creating from a Project, optionally set the channel if provided
+    if @parent.is_a?(Project) && comment_params[:channel_id].present?
       @comment.channel = Channel.find(comment_params[:channel_id])
     end
   
-    # 3) Save and redirect
     if @comment.save
-      if @project
-        redirect_to @project, notice: 'Comment was successfully added.'
-      elsif @channel
-        redirect_to @channel, notice: 'Comment was successfully added.'
-      else
-        redirect_to root_path, notice: 'Comment was successfully added.'
-      end
+      redirect_to @parent, notice: 'Comment was successfully added.'
     else
-      # Handle failure
-      if @project
-        redirect_to @project, alert: 'Failed to add comment.'
-      elsif @channel
-        redirect_to @channel, alert: 'Failed to add comment.'
-      else
-        redirect_to root_path, alert: 'Failed to add comment.'
-      end
+      redirect_to @parent, alert: 'Failed to add comment.'
     end
-  end
+  end    
   
-
   def edit
     @project = Project.find(params[:project_id])
     @comment = @project.comments.find(params[:id])
@@ -84,7 +57,7 @@ class CommentsController < ApplicationController
 
   def destroy #DELETE action that deletes a comment
     @comment.destroy
-    redirect_to @project, notice: 'Comment was successfully deleted.' #When deletion is successful, it redirects to the index view of the project's comments
+    redirect_to @parent, notice: 'Comment was successfully deleted.' #When deletion is successful, it redirects to the index view of the project's comments
   end
 
   def weekly
@@ -107,14 +80,17 @@ class CommentsController < ApplicationController
   private #This section defines helper methods that are used internally within the controller. These methods aren't accessible from outside the controller (that's why they're marked as private), but they are crucial for managing the flow of data. The set_projects and set_comment methods used before actions (lines 2 and 3) are defined below
   
     #none of the @project syntax above the private line would make sense or mean anything if it wasn't for this line below
-    def set_project
+    def set_parent
       if params[:project_id].present?
-        @project = Project.find(params[:project_id])
+        @parent = Project.find(params[:project_id])
+      elsif params[:channel_id].present?
+        @parent = Channel.find(params[:channel_id])
       end
     end
 
-    def set_comment #This method finds a specific comment within the context of the current project.
-      @comment = @project.comments.find(params[:id])
+    # Now that @parent is set, we find the comment from its association.
+    def set_comment
+      @comment = @parent.comments.find(params[:id])
     end
       
     def comment_params #Strong parameters: permit only the necessary attributes to prevent mass assignment vulnerabilities
